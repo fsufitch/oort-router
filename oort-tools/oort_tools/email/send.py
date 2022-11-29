8330c81df43c21a715e8d2c9f8a2684e05404727
Filip Sufitchi <fsufitchi@gmail.com>
2022-11-29 00:53:01 -0500

    Rename arg

--- oort-tools/oort_tools/email/send.py
+++ oort-tools/oort_tools/email/send.py
@@ -20,7 +20,7 @@ ENCODING = "utf-8"
 def _build_argument_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description="Send an email using AWS SES")
     parser.add_argument(
-        "recipient", metavar="EMAIL", nargs="+", help="recipients of the email"
+        "recipient", metavar="to_email", nargs="+", help="recipients of the email"
     )
     parser.add_argument("subject", help="subject of the email")
 
